2db48bc86933749d6357711801e9aeaf66d71b24
ludo6577 <ludo6577@gmail.com>
2014-12-02 19:46:01 +0100

    commit

---
+++ JavascriptFrameworksTest/script/hugeClientsList.js
@@ -0,0 +1,296 @@
+
+var Genders = [
+	{
+		id: 0,
+		key: "Mr",
+		value: "Monsieur"
+	},
+	{
+		id: 1,
+		key: "Mme",
+		value: "Madame"
+	}
+]
+
+
+var ClientsList = [
+	{
+		id: 0,
+		gender: Genders[0],
+		name: "Jean",
+		age: 25,
+	},
+	{
+		id: 1,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 2,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 3,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 4,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 5,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 6,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 7,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 8,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 9,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 10,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 11,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 12,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 13,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 14,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 15,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 16,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 17,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 18,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 19,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 20,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 21,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 22,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+		{
+		id: 23,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 24,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 25,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 26,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 27,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 28,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 29,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 30,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 31,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 32,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 33,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 34,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 35,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 36,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 37,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 38,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 39,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 40,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 41,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 42,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 43,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 44,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 45,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 46,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 47,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 48,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 49,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},	{
+		id: 50,
+		gender: Genders[1],
+		name: "Juliette",
+		age: 30,
+	},
+]
